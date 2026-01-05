9776f5ac231222f7a732d399c537bdfb06891601
sanatrath123 <rathsanantakumar@gmail.com>
2026-01-05 14:12:22 +0530

    changed the git webhook env for spwan

--- routes/gitWebhook.js
+++ routes/gitWebhook.js
@@ -8,7 +8,20 @@ router.post("/", async(req,res,next)=>{
 let childStream 
 console.log(req.body)
 const repoName = req.body.repository.name
-if(repoName=="StoreX_Server_Backup")  childStream = spawn('bash', ["/home/ubuntu/StoreX_Bash.sh"])
+
+
+if(repoName=="StoreX_Server_Backup") childStream = spawn(
+  'bash',
+  ['/home/ubuntu/Clinet_Bash.sh'],
+  {
+    env: {
+      ...process.env,          // VERY IMPORTANT
+      AWS_PROFILE: 'sanat08',  // your profile name
+      AWS_REGION: 'ap-south-1' // optional but recommended
+    }
+  }
+)
+
 if(repoName=="StoreX_Client_Backup")childStream = spawn('bash', ["/home/ubuntu/Clinet_Bash.sh"])
 
 //  const childStream = spawn('bash', ["/c/Users/Backend\ NODE-JS/section-21/15_Automating_Deployments_with_Bash_Scripting/demo.sh"])
@@ -25,6 +38,10 @@ childStream.on("close", code =>{
         console.log("error while running the bash file")
     }
     return res.status(200).json({msg:"done executing the bash file"})
+})
+
+childStream.stderr.on("data",(chunk)=>{
+    process.stderr.write(chunk)
 })
 
 
