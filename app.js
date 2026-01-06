d9d9ca8e4a8cb97a494f589b0c5a647e84b03228
sanatrath123 <rathsanantakumar@gmail.com>
2026-01-06 16:14:40 +0530

    demo changed 1

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(express.json({
 app.use(cookieParser(Config.Cookie_Secreate))
 
   app.get("/",(req,res)=>{
-    res.json({msg:"hello from Store-X app"})
+    res.json({msg:"hello from Store-X app changed"})
   })
 
 app.use("/directory",CheeckAuth, directoryRouter)
