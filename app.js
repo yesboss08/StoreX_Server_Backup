cab0d27c104b9e02db5ada0ec2cb614e6f353147
sanatrath123 <rathsanantakumar@gmail.com>
2026-01-09 16:54:52 +0530

    test changed 2

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(express.json({
 app.use(cookieParser(Config.Cookie_Secreate))
 
   app.get("/",(req,res)=>{
-    res.json({msg:"hello from mostorage app modified 🌹"})
+    res.json({msg:"hello from mostorage app "})
   })
 
 app.use("/directory",CheeckAuth, directoryRouter)
