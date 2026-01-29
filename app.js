217d59dde4ca9b23218eac3365e867700802dc67
sanatrath123 <rathsanantakumar@gmail.com>
2026-01-30 04:55:38 +0530

    update the main.yml file

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(express.json({
 app.use(cookieParser(Config.Cookie_Secreate))
 
   app.get("/",(req,res)=>{
-    res.json({msg:"hello from mostorage app 🌹🌹🌹 "})
+    res.json({msg:"hello from mostorage app 🥰  "})
   })
 
 app.use("/directory",CheeckAuth, directoryRouter)
